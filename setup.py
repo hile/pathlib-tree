15871c479305a871207265277e7a8da735e9b4ac
Ilkka Tuohela <ilkka.tuohela@codento.com>
2021-12-15 18:08:17 +0200

    Depend on latest cli-toolkit

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     entry_points={},
     install_requires=(
-        'cli-toolkit>=1.1.1',
+        'cli-toolkit>=1.1.3',
         'filemagic==1.6',
     ),
     classifiers=[
